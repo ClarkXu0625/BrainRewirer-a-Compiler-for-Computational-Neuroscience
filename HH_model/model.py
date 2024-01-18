a3581c96b927c2db5ea2450502db7ecc24722639
Clark Xu <yilinxu@brandeis.edu>
2024-01-18 16:10:05 -0500

    model update function finish

--- HH_model/model.py
+++ HH_model/model.py
@@ -2,21 +2,31 @@ import numpy as np
 from brian2 import *
 
 
+
 class HHModel:
     """The HHModel tracks conductances of 3 channels to calculate Vm"""
 
+    class Gate:
+        """The Gate object manages a channel's kinetics and open state"""
+        alpha, beta, state = 0, 0, 0
 
+        def update(self):
+            alphaState = self.alpha * (1-self.state)
+            betaState = self.beta * self.state
+            self.state += alphaState - betaState
 
-    # m, n, h = Gate(), Gate(), Gate()
+        def setInfiniteState(self):
+            self.state = self.alpha / (self.alpha + self.beta)
 
 
     def __init__(self, group_name, startingVoltage):
         self.group = group_name
         self.Vm = startingVoltage
-        self._UpdateGateTimeConstants(startingVoltage)
-        # self.m.setInfiniteState()
-        # self.n.setInfiniteState()
-        # self.h.setInfiniteState()
+        self._update_gate_time_constants(startingVoltage)
+        self.m, self.n, self.h = self.Gate(), self.Gate(), self.Gate()
+        self.m.setInfiniteState()
+        self.n.setInfiniteState()
+        self.h.setInfiniteState()
 
     def synaptic_param(self, tau_r, tau_d, V_syn, V_0):
         # set up synaptic parameter
@@ -25,8 +35,6 @@ class HHModel:
         self.V_syn = V_syn
         self.V_0 = V_0
         
-
-
     def model_param(self, C_m, V_K, V_Ca, V_Na, V_L, V_T, g_K_max, g_M_max, g_Ca_max, g_Na_max, g_L, tau_max):
         # set up model parameters
         self.C_m = C_m              # membrane charge
@@ -43,8 +51,10 @@ class HHModel:
         self.tau_max = tau_max
         
 
-    def _UpdateGateTimeConstants(self, Vm, V_T):
+    def _update_gate_time_constants(self):
         """Update time constants of all gates based on the given Vm"""
+        V_T = self.V_T
+        Vm = self.Vm
 
         self.n.alpha = (Vm-V_T-15) * .032/ (np.exp((Vm-V_T-15)/5)-1)
         self.m.alpha = (Vm-V_T-13) * .32/ (np.exp((Vm-V_T-13)/4)-1)
@@ -54,27 +64,20 @@ class HHModel:
         self.m.beta = .28*np.exp((Vm-V_T-40)/5-1)
         self.h.beta = 4/(np.exp((Vm-V_T-40)/5)+1)
 
-    def _UpdateCellVoltage(self, stimulusCurrent, deltaTms):
+    def _update_voltage(self, stimulusCurrent):
         """calculate channel currents using the latest gate time constants"""
         pass
-        # self.INa = np.power(self.m.state, 3) * self.gNa * \
-        #     self.h.state*(self.Vm-self.ENa)
-        # self.IK = np.power(self.n.state, 4) * self.gK * (self.Vm-self.EK)
-        # self.IKleak = self.gKleak * (self.Vm-self.EKleak)
-        # Isum = stimulusCurrent - self.INa - self.IK - self.IKleak
-        # self.Vm += deltaTms * Isum / self.Cm
 
-    def _UpdateGateStates(self, deltaTms):
+
+    def _update_gate_states(self):
         """calculate new channel open states using latest Vm"""
-        pass
-        # self.n.update(deltaTms)
-        # self.m.update(deltaTms)
-        # self.h.update(deltaTms)
+        self.n.update()
+        self.m.update()
+        self.h.update()
 
-    def iterate(self, stimulusCurrent, deltaTms):
-        pass
-        # self._UpdateGateTimeConstants(self.Vm)
-        # self._UpdateCellVoltage(stimulusCurrent, deltaTms)
-        # self._UpdateGateStates(deltaTms)
+    def iterate(self, stimulus_current):
+        self._update_gate_time_constants()
+        self._update_voltage(stimulus_current)
+        self._update_gate_states()
     
     #def _UpdateGateTimeConstant_m()
--- HH_model/neuron.py
+++ HH_model/neuron.py
@@ -2,25 +2,16 @@ from brian2 import *
 import numpy as np
 from model import HHModel
 
-class Gate:
-    """The Gate object manages a channel's kinetics and open state"""
-    alpha, beta, state = 0, 0, 0
 
-    def update(self, deltaTms):
-        alphaState = self.alpha * (1-self.state)
-        betaState = self.beta * self.state
-        self.state += deltaTms * (alphaState - betaState)
-
-    def setInfiniteState(self):
-        self.state = self.alpha / (self.alpha + self.beta)
 
 
 class FS(HHModel):
     def __init__(self):
         super.__init__('FS')
-        self.m, self.n, self.h = Gate(), Gate(), Gate()
+        
 
-    def updata_voltage(self, Vm, I_inj):
+    def _update_voltage(self, I_inj):
+        Vm = self.Vm
         m, n, h = self.m.state, self.n.state, self.h.state
         dVdt = (1/self.C_m)*(I_inj - self.g_K_max*(n**4)*(Vm-self.V_K) 
                              - self.g_Na_max*(m**3)*h*(Vm-self.V_Na) 
@@ -32,16 +23,17 @@ class FS(HHModel):
 class RSA(HHModel):
     def __init__(self):
         super.__init__("RSA")
-        self.m, self.n, self.h = Gate(), Gate(), Gate()
         self.p = 0
 
-    def update_p(self, Vm):
+    def _update_p(self):
+        Vm =self.Vm
         p_inf = 1/(np.exp((-Vm-335)/10)+1)
         tau_p = self.tau_max/(3.3*np.exp((Vm+35)/20) + np.exp((-Vm-35)/20))
         dpdt = (p_inf - self.p)/tau_p
         self.p += dpdt
 
-    def updata_voltage(self, Vm, I_inj):
+    def _update_voltage(self, I_inj):
+        Vm = self.Vm
         m, n, h = self.m.state, self.n.state, self.h.state
         dVdt = (1/self.C_m)*(I_inj - self.g_K_max*(n**4)*(Vm-self.V_K) 
                             - self.g_M_max*self.p*(Vm-self.V_K)
@@ -49,27 +41,38 @@ class RSA(HHModel):
                             - self.g_L*(Vm-self.V_L))
         return Vm+dVdt
 
+    def _update_gate_states(self):
+        '''Different from FS, RSA also needs to update variable p'''
+        super._update_gate_states()
+        self.updata_p()
 
 class IB(HHModel):
     def __init__(self):
         super().__init__("IB")
-        self.m, self.n, self.h, self.q, self.s= Gate(), Gate(), Gate(), Gate(), Gate()
+        self.p = 0
+        self.q, self.s = super.Gate(), super.Gate()     # two additional calcium gating variable
+        self.q.setInfiniteState()
+        self.s.setInfiniteState()
 
-    def update_p(self, Vm):
+    def _update_p(self):
+        Vm = self.Vm
+
         p_inf = 1/(np.exp((-Vm-335)/10)+1)
         tau_p = self.tau_max/(3.3*np.exp((Vm+35)/20) + np.exp((-Vm-35)/20))
         dpdt = (p_inf - self.p)/tau_p
         self.p += dpdt
 
-    def _UpdateGateTimeConstants(self, Vm, V_T):
+    def _update_gate_time_constants(self):
         '''IB neurons have two more gating variables: q and s'''
-        super._UpdateGateTimeConstants(Vm, V_T)
+        super._update_gate_time_constants()
+        Vm = self.Vm
         self.q.alpha = .0055*(-27-Vm)/(np.exp((-27-Vm)/3.8)-1)
         self.s.alpha = .000457*np.exp((-13-Vm)/50)
         self.s.beta = .0065/(np.exp((-15-Vm)/28)+1)
         self.q.beta = .94*np.exp((-75-Vm)/17)
 
-    def updata_voltage(self, Vm, I_inj):
+    def _update_voltage(self, I_inj):
+        Vm = self.Vm
         m, n, h, q, s = self.m.state, self.n.state, self.h.state, self.q.state, self.s.state
         dVdt = (1/self.C_m)*(I_inj - self.g_K_max*(n**4)*(Vm-self.V_K) 
                             - self.g_M_max*self.p*(Vm-self.V_K)
@@ -77,3 +80,10 @@ class IB(HHModel):
                             - self.g_Na_max*(m**3)*h*(Vm-self.V_Na) 
                             - self.g_L*(Vm-self.V_L))
         return Vm+dVdt
+    
+    def _update_gate_states(self):
+        '''IB neurons also needs to update variable p, calcium gating variable q and s'''
+        super._update_gate_states()
+        self.updata_p()
+        self.q.update()
+        self.s.update()
