db3b45773d74d358a02fe72f5221ba837a3062be
Clark Xu <yilinxu@brandeis.edu>
2024-01-18 14:52:45 -0500

    reorganize class structure

--- HH_model/model.py
+++ HH_model/model.py
@@ -1,5 +1,4 @@
 import numpy as np
-import neuron
 from brian2 import *
 
 
@@ -18,18 +17,41 @@ class HHModel:
         def setInfiniteState(self):
             self.state = self.alpha / (self.alpha + self.beta)
 
-    ENa, EK, EKleak = 115, -12, 10.6
-    gNa, gK, gKleak = 120, 36, 0.3
-    m, n, h = Gate(), Gate(), Gate()
-    Cm = 1
-    
 
-    def __init__(self, startingVoltage=0):
+    # m, n, h = Gate(), Gate(), Gate()
+
+
+    def __init__(self, group_name, startingVoltage):
+        self.group = group_name
         self.Vm = startingVoltage
         self._UpdateGateTimeConstants(startingVoltage)
         self.m.setInfiniteState()
         self.n.setInfiniteState()
         self.h.setInfiniteState()
+
+    def synaptic_param(self, tau_r, tau_d, V_syn, V_0):
+        # set up synaptic parameter
+        self.tau_r = tau_r
+        self.tau_d = tau_d
+        self.V_syn = V_syn
+        self.V_0 = V_0
+        
+
+
+    def model_param(self, C_m, V_K, V_Ca, V_Na, V_L, V_T, g_K_max, g_M_max, g_Na_max, g_L, tau_max):
+        # set up model parameters
+        self.C_m = C_m              # membrane charge
+        self.V_K = V_K              # potassium nernst voltage
+        self.V_Ca = V_Ca            # calcium nernst voltage
+        self.V_Na = V_Na            # sodium nernst voltage
+        self.V_L = V_L              # leaky nernst voltage
+        self.V_T = V_T              # threshold adjustment constant
+        self.g_K_max = g_K_max
+        self.g_M_max = g_M_max
+        self.g_Na_max = g_Na_max
+        self.g_L = g_L
+        self.tau_max = tau_max
+        
 
     def _UpdateGateTimeConstants(self, Vm, V_T):
         """Update time constants of all gates based on the given Vm"""
@@ -40,29 +62,26 @@ class HHModel:
         self.n.beta = .5*np.exp((Vm-V_T-10*mV)/40)
         self.m.beta = .28*np.exp((Vm-V_T-40)/5-1)
         self.h.beta = 4/(np.exp((Vm-V_T-40)/5)+1)
-        # self.n.alpha = .01 * ((10-Vm) / (np.exp((10-Vm)/10)-1))
-        # self.n.beta = .125*np.exp(-Vm/80)
-        # self.m.alpha = .1*((25-Vm) / (np.exp((25-Vm)/10)-1))
-        # self.m.beta = 4*np.exp(-Vm/18)
-        # self.h.alpha = .07*np.exp(-Vm/20)
-        # self.h.beta = 1/(np.exp((30-Vm)/10)+1)
 
     def _UpdateCellVoltage(self, stimulusCurrent, deltaTms):
         """calculate channel currents using the latest gate time constants"""
-        self.INa = np.power(self.m.state, 3) * self.gNa * \
-            self.h.state*(self.Vm-self.ENa)
-        self.IK = np.power(self.n.state, 4) * self.gK * (self.Vm-self.EK)
-        self.IKleak = self.gKleak * (self.Vm-self.EKleak)
-        Isum = stimulusCurrent - self.INa - self.IK - self.IKleak
-        self.Vm += deltaTms * Isum / self.Cm
+        pass
+        # self.INa = np.power(self.m.state, 3) * self.gNa * \
+        #     self.h.state*(self.Vm-self.ENa)
+        # self.IK = np.power(self.n.state, 4) * self.gK * (self.Vm-self.EK)
+        # self.IKleak = self.gKleak * (self.Vm-self.EKleak)
+        # Isum = stimulusCurrent - self.INa - self.IK - self.IKleak
+        # self.Vm += deltaTms * Isum / self.Cm
 
     def _UpdateGateStates(self, deltaTms):
         """calculate new channel open states using latest Vm"""
-        self.n.update(deltaTms)
-        self.m.update(deltaTms)
-        self.h.update(deltaTms)
+        pass
+        # self.n.update(deltaTms)
+        # self.m.update(deltaTms)
+        # self.h.update(deltaTms)
 
     def iterate(self, stimulusCurrent, deltaTms):
-        self._UpdateGateTimeConstants(self.Vm)
-        self._UpdateCellVoltage(stimulusCurrent, deltaTms)
-        self._UpdateGateStates(deltaTms)
+        pass
+        # self._UpdateGateTimeConstants(self.Vm)
+        # self._UpdateCellVoltage(stimulusCurrent, deltaTms)
+        # self._UpdateGateStates(deltaTms)
--- HH_model/neuron.py
+++ HH_model/neuron.py
@@ -1,47 +1,25 @@
 from brian2 import *
 import numpy as np
-
-# base classs
-class neuron:
-    def __init__(self, group_name):
-        self.group = group_name
-
-    def synaptic_param(self, tau_r, tau_d, V_syn, V_0):
-        # set up synaptic parameter
-        self.tau_r = tau_r
-        self.tau_d = tau_d
-        self.V_syn = V_syn
-        self.V_0 = V_0
+from model import HHModel
 
 
-    def model_param(self, C_m, V_K, V_Ca, V_Na, V_L, V_T, g_K_max, g_M_max, g_Na_max, g_L, tau_max):
-        # set up model parameters
-        self.C_m = C_m              # membrane charge
-        self.V_K = V_K              # potassium nernst voltage
-        self.V_Ca = V_Ca            # calcium nernst voltage
-        self.V_Na = V_Na            # sodium nernst voltage
-        self.V_L = V_L              # leaky nernst voltage
-        self.V_T = V_T              # threshold adjustment constant
-        self.g_K_max = g_K_max
-        self.g_M_max = g_M_max
-        self.g_Na_max = g_Na_max
-        self.g_L = g_L
-        self.tau_max = tau_max
-
-
-
-class FS(neuron):
+class FS(HHModel):
     def __init__(self):
         super.__init__('FS')
         super.synaptic_param()
         super.model_param()
 
+    def updata_voltage(self, Vm, n, m, h, I_inj):
+        dVdt = (1/self.C_m)*(I_inj - self.g_K_max*(n**4)*(Vm-self.V_K) - self.g_Na_max*(m**3)*h*(Vm-self.V_Na) - self.g_L*(Vm-self.V_L))
+        return Vm+dVdt
+    
 
-class RSA(neuron):
+
+class RSA(HHModel):
     def __init__(self):
         super.__init__()
 
 
-class IB(neuron):
+class IB(HHModel):
     def __init__(self):
         super().__init__()
