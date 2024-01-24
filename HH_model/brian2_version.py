98ad16f032ee6449ab5cef958548d6a909cb44f1
Clark Xu <yilinxu@brandeis.edu>
2024-01-24 12:54:42 -0500

    update brian2 version

--- HH_model/brian2_version.py
+++ HH_model/brian2_version.py
@@ -23,3 +23,15 @@ tau_r = 0.5 * msecond
 tau_d = 8 * msecond
 V_syn = 20 * mvolt
 V_0 = -20 * mvolt
+
+
+eqs_HH = '''
+dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
+dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
+    (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
+    (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
+dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
+    (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+I : amp
+'''
