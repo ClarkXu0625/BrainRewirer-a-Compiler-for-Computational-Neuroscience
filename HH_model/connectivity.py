ba17e8b8a839306fb56714900be095329a1e891e
Clark Xu <yilinxu@brandeis.edu>
2024-01-24 13:32:00 -0500

    update connectivity

--- HH_model/connectivity.py
+++ HH_model/connectivity.py
@@ -6,8 +6,8 @@ from neuron import FS, RSA, IB
 dt = 1e-4                               # size of a time step, 0.1ms
 simulation_time = 10                    # total seconds of simulation
 t = np.arange(0, simulation_time, dt)   # time vector
+Nt = t.size                             # number of time steps
 voltage = np.zeros(t.shape)
-
 
 neuron_list = []
 for i in range(30):
@@ -22,4 +22,19 @@ for i in range(30):
     IB_neuron = IB(dt, IB_PARAM)
     neuron_list.append(IB_neuron)
 
-print(neuron_list.size)
+n = len(neuron_list)                        # size of the network
+connectivity_matrix = np.zeros((n,n))       # connectivity matrix
+voltage_matrix = np.zeros((Nt, n))
+
+def inj_current(V):
+    pass
+
+# iterate through time
+for i in range(Nt):
+    current = inj_current(voltage_matrix[i,:])
+    for j in range(n):
+        neuron = neuron_list[j]       
+        neuron.iterate(current[j])
+        voltage_matrix[i,j] = neuron.Vm
+
+
